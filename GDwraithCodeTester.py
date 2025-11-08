24e7e01b3404fb517a7de707325780f4c2fc5ccc
oxyrv <161810711+oxyrv@users.noreply.github.com>
2025-11-08 09:56:50 +0100

    Update GDwraithCodeTester.py

--- GDwraithCodeTester.py
+++ GDwraithCodeTester.py
@@ -1,4 +1,40 @@
 from requests import post
-while True:
-    if post("https://www.boomlings.com/database/getGJSecretReward.php",data={"rewardKey":input("Type in a wraith code : ").replace(" ",""),"udid":"1","chk":"g6f2zAAwHCgMG","secret":"Wmfd2893gb7"},headers={"User-Agent":""}).content.decode()!="-1":
-        print("Code Works!")
+import colorama
+from colorama import Fore
+import os
+from pystyle import *
+import sys
+
+colorama.init(autoreset=False)
+
+banner = Fore.MAGENTA + """
+ ▄█     █▄     ▄████████    ▄████████  ▄█      ███        ▄█    █▄    
+███     ███   ███    ███   ███    ███ ███  ▀█████████▄   ███    ███   
+███     ███   ███    ███   ███    ███ ███▌    ▀███▀▀██   ███    ███   
+███     ███  ▄███▄▄▄▄██▀   ███    ███ ███▌     ███   ▀  ▄███▄▄▄▄███▄▄ 
+███     ███ ▀▀███▀▀▀▀▀   ▀███████████ ███▌     ███     ▀▀███▀▀▀▀███▀  
+███     ███ ▀███████████   ███    ███ ███      ███       ███    ███   
+███ ▄█▄ ███   ███    ███   ███    ███ ███      ███       ███    ███   
+ ▀███▀███▀    ███    ███   ███    █▀  █▀      ▄████▀     ███    █▀    
+              ███    ███                                              """ + "\n\n" + Fore.CYAN + "  -------> Original By RigMatter12 | ReDesigned By Oxyru <-------\n" + Fore.RESET
+
+if __name__ == "__main__":
+    os.system("title Wraith Code Tester v1" if os.name == "nt" else "")
+    os.system("cls" if os.name == "nt" else "clear")
+    Cursor.HideCursor()
+    print(Center.XCenter(banner))
+    while True:
+        Cursor.HideCursor()
+        key = input(Fore.CYAN + ' [?] Type in a wraith code : ').replace(' ', '')
+        Cursor.HideCursor()
+        if not key == "exit":
+            print(Fore.YELLOW + ' [i] Testing This Code...', end='\r')
+            Cursor.HideCursor()
+            if post('https://www.boomlings.com/database/getGJSecretReward.php', data={'rewardKey': key, 'udid': '1', 'chk': 'g6f2zAAwHCgMG', 'secret': 'Wmfd2893gb7'}, headers={'User-Agent': ''}).content.decode() != '-1':
+                print(Fore.GREEN + ' [+] Code Works!         ')
+                Cursor.HideCursor()
+            else:
+                print(Fore.RED + ' [x] This Code Doesn\'t Works!')
+                Cursor.HideCursor()
+        else:
+            sys.exit()
